c6f02d80c2a1598b357e6977a1bf64988120855b
Michael Hettmer <mail@michael-hettmer.de>
2019-09-12 00:04:38 +0200

    Implement the first few hooks with tests

--- src/usePrevious.test.ts
+++ src/usePrevious.test.ts
@@ -1,8 +1,60 @@
-import usePrevious from './usePrevious';
+import { renderHook, act } from '@testing-library/react-hooks';
+import usePrevious, {
+    usePreviousNumber,
+    usePreviousNumberArray,
+    usePreviousBoolean,
+    usePreviousBooleanArray,
+} from './usePrevious';
+import { useState } from 'react';
 
 describe('test react-hooks-use-previous', () => {
     it('should call usePrevious successfully', () => {
-        const result = usePrevious();
-        expect(result).toBeUndefined();
+        const value = 0;
+        const { result } = renderHook(() => usePrevious<number>(value, 0));
+        expect(result.current).toBe(0);
+    });
+
+    it('should call usePreviousNumber successfully and assign the correct default value', () => {
+        const value = 0;
+        const { result } = renderHook(() => usePreviousNumber(value));
+        expect(result.current).toBe(0);
+    });
+
+    it('should call usePreviousNumberArray successfully and assign the correct default value', () => {
+        const value = [0];
+        const { result } = renderHook(() => usePreviousNumberArray(value));
+        expect(result.current).toEqual([]);
+    });
+
+    it('should call usePreviousBoolean successfully and assign the correct default value', () => {
+        const value = false;
+        const { result } = renderHook(() => usePreviousBoolean(value));
+        expect(result.current).toBe(false);
+    });
+
+    it('should call usePreviousBooleanArray successfully and assign the correct default value', () => {
+        const value = [false];
+        const { result } = renderHook(() => usePreviousBooleanArray(value));
+        expect(result.current).toEqual([]);
+    });
+
+    it('should return the previous value when updating the associated state variable', () => {
+        const useTestHook = () => {
+            const [value, setValue] = useState(77);
+            const prevValue = usePrevious<number>(value, 0);
+            return { value, setValue, prevValue };
+        };
+
+        const { result } = renderHook(() => useTestHook());
+
+        expect(result.current.prevValue).toBe(0);
+        expect(result.current.value).toBe(77);
+
+        act(() => {
+            result.current.setValue(33);
+        });
+
+        expect(result.current.prevValue).toBe(77);
+        expect(result.current.value).toBe(33);
     });
 });
--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -1,5 +1,23 @@
-const usePrevious = () => {
-    return undefined;
+import { useRef, useEffect } from 'react';
+
+export const usePreviousNumber = (value: number, initialPreviousValue = 0) =>
+    usePrevious<number>(value, initialPreviousValue);
+
+export const usePreviousNumberArray = (value: number[], initialPreviousValue: number[] = []) =>
+    usePrevious<number[]>(value, initialPreviousValue);
+
+export const usePreviousBoolean = (value: boolean, initialPreviousValue = false) =>
+    usePrevious<boolean>(value, initialPreviousValue);
+
+export const usePreviousBooleanArray = (value: boolean[], initialPreviousValue: boolean[] = []) =>
+    usePrevious<boolean[]>(value, initialPreviousValue);
+
+const usePrevious = <T>(value: T, initialPreviousValue: T) => {
+    const ref = useRef<T>(initialPreviousValue);
+    useEffect(() => {
+        ref.current = value;
+    }, [value]);
+    return ref.current;
 };
 
 export default usePrevious;
