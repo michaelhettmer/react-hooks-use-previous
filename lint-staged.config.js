9e1229d37cb901c8245e79accc0bf75f1017b170
Michael Hettmer <mail@michael-hettmer.de>
2020-04-16 22:01:01 +0200

    fix: update function signatures to pass new prettier v2 rules

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     '*.{js,jsx,json,jsonc}': ['npm run lint:fix'],
-    '**/.circleci/config.yml': filenames => filenames.map(filename => `npm run lint:circleci "${filename}"`),
+    '**/.circleci/config.yml': (filenames) => filenames.map((filename) => `npm run lint:circleci "${filename}"`),
 };
